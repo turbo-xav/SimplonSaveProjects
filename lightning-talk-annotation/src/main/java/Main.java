ef152ca3b8c4ddc8bfc67dba8aafa3f8e6ccc3ac
Turbo Xav <xavier.tagliarino@gmail.com>
2018-05-17 15:27:08 +0200

    lightling talk réfléxivité 2 - annotation

--- lightning-talk-annotation/src/main/java/Main.java
+++ lightning-talk-annotation/src/main/java/Main.java
@@ -57,7 +57,7 @@ public class Main {
 		
 		//Dynamiquement
 		runMethode(objetWithAnnotation,"methode1");
-		runMethode(objetWithAnnotation,"methode2");	
+		runMethode(objetWithAnnotation,"methode2");
 		
 	}
 	
