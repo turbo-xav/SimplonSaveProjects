92f386465fc02e4557d6600556e3d2dd9aa1f4c1
Turbo Xav <xavier.tagliarino@gmail.com>
2018-06-07 12:48:10 +0200

    angular material

--- angularmaterial/src/app/app.module.ts
+++ angularmaterial/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatCheckboxModule, MatInputModule, MatDatepickerModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,8 +12,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
